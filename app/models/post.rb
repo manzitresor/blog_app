865c9107b2701843829fa127c16160cbcd22dfea
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-20 17:30:05 +0200

    add post counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+    def posts_counter
+        author.increment!(posts_counter)
+    end
+    def recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
 end
