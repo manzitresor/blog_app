09d9b8c99a41d7ea52808f2466894cd007f2d4bf
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-20 17:34:40 +0200

    add recent comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
     has_many :comments
     has_many :likes
     def posts_counter
-        author.increment!(posts_counter)
+        author.increment!(:postsCounter)
     end
     def recent_comments
         comments.order(created_at: :desc).limit(5)
