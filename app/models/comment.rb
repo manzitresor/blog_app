934002257c7da7ea977daf4602ccf1bf819ab1b6
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-20 17:47:16 +0200

    add comment counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_save :comment_counter
+  
+  def comment_counter
+    post.increment!(:commentsCounter)
+  end
 end
