d1d1109692b88c4baec3494b9feaa610a1946559
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-21 19:48:03 +0200

    add comment specs

---
+++ spec/model/comment_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do 
+    let(:user) {create(:user) }
+    let(:post) {create(:post,author:user) }
+    subject {Comment.new(user:user, post:post)}
+    before{subject.save}
+    it"#comment_counter" do
+        expect{subject.save}.to change{post.reload.commentsCounter}.by(1)
+    end
+end
