8844ba69fc41a3059ccb57dac4a2d2464b6c1dbe
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-28 18:54:17 +0200

    Add rubocop checker

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,8 @@ class PostsController < ApplicationController
   def index
     @users = [User.find(params[:user_id])]
     @posts = @users[0].posts
-   end
+  end
+
   def show
     @posts = [Post.find(params[:id])]
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
   def show
     @users = [User.find(params[:id])]
     @posts = @users[0].recent_posts
