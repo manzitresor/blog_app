d77302b0eb7a859ee08a291d180761284ad7d0a3
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-29 10:46:28 +0200

    Add new and create post contollers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,18 @@ class PostsController < ApplicationController
 
   def show
     @posts = [Post.find(params[:id])]
+    @comment = Comment.new
+  end
+  def new
+    @post = Post.new
+  end
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author(id: current_user)
+    if @post.save 
+      redirect_to user_path(id: current_user)
+    else
+      render :new
+    end
   end
 end
