7d01a38ae3c17b1489d5094654e7ae01f2d0f32b
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-09-29 15:36:13 +0200

    Add rubocop correction

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,13 +9,15 @@ class PostsController < ApplicationController
     @comment = Comment.new
     @like = Like.new
   end
+
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(params.require(:post).permit(:title, :text))
     @post.author = current_user
-    if @post.save 
+    if @post.save
       redirect_to user_posts_path(id: current_user)
     else
       render :new
