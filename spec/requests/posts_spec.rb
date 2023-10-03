8994633f24b64068792269707740afb79683004b
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-03 13:26:16 +0200

    Add request spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,9 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
+  before :each do
+    @user_one = User.create(
+      id: 1,
+      name: 'Cosmas',
+      photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+      bio: 'Web developer from Uganda',
+      post_counter: 0
+    )
+
+    @posts = [
+      @post1 = Post.create(
+        author: @user_one,
+        title: 'Blog1',
+        text: 'This is my first post',
+        comments_counter: 0,
+        likes_counter: 0
+      ),
+      @post2 = Post.create(
+        author: @user_one,
+        title: 'Blog2',
+        text: 'This is my second post',
+        comments_counter: 0,
+        likes_counter: 0
+      )
+    ]
+  end
+
   describe 'GET /index' do
     before :each do
-      get '/users/:user_id/posts'
+      get user_posts_url(user_id: @user_one.id)
     end
     it 'should returns http success' do
       expect(response).to have_http_status(:success)
@@ -11,22 +38,16 @@ RSpec.describe 'Posts', type: :request do
     it 'should render index' do
       expect(response).to render_template(:index)
     end
-    it 'should includes correct placeholder text' do
-      expect(response.body).to include('User posts')
-    end
   end
   describe 'GET /show' do
     before :each do
-      get '/users/:user_id/posts/:id'
+      get user_post_path(@user_one, @post1)
     end
     it 'should returns http success' do
       expect(response).to have_http_status(:success)
     end
     it 'should render index' do
       expect(response).to render_template(:show)
-    end
-    it 'should includes correct placeholder text' do
-      expect(response.body).to include('User posts with Id')
     end
   end
 end
