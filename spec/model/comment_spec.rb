1a81ff613d3aa4c615afc5b4bedc3c4eb61455dc
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-03 12:38:24 +0200

    Add module specs

--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   let(:user) { create(:user) }
   let(:post) { create(:post, author: user) }
-  subject { Comment.new(user:, post:) }
-  before { subject.save }
+  subject { Comment.new(user: user, post: post) }
+
   it '#comment_counter' do
-    expect { subject.save }.to change { post.reload.commentsCounter }.by(1)
+    expect { subject.save }.to change { post.reload.comments_counter }.by(1)
   end
 end
