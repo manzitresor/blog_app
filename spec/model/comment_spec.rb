622014bdbd79e7e82cec9dc381bd680e111ce6a5
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-10-03 12:41:01 +0200

    Add solve rubocop error

--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   let(:user) { create(:user) }
   let(:post) { create(:post, author: user) }
-  subject { Comment.new(user: user, post: post) }
+  subject { Comment.new(user:, post:) }
 
   it '#comment_counter' do
     expect { subject.save }.to change { post.reload.comments_counter }.by(1)
